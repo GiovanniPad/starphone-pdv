230ab461c48ebad26337e9243aecda23b2c2b3d2
Giovanni Padilha <giovanni.padilha@outlook.com>
2025-11-13 20:56:21 -0300

    feat(models): Relation between User and Product Models
    Make an relation between User and Product Model to register who
    created and updated a product. Just for information purpose.

--- src/starphone_api/models/product.py
+++ src/starphone_api/models/product.py
@@ -1,8 +1,11 @@
 from decimal import Decimal
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
 from sqlmodel import Field, ForeignKey, Relationship, SQLModel
 
 from starphone_api.models.category import Category
+
+if TYPE_CHECKING:
+    from starphone_api.models.user import User
 
 
 class Product(SQLModel, table=True):
@@ -12,6 +15,16 @@ class Product(SQLModel, table=True):
     quantity: int = Field(default=0, nullable=False)
     cost_value: Decimal = Field(nullable=False)
     profit_value: Decimal = Field(nullable=False)
+    created_by_id: int = Field(foreign_key="user.id", nullable=False)
+    updated_by_id: Optional[int] = Field(foreign_key="user.id", default=None, nullable=True)
 
     # Relacionamento com Category
     category: Optional[Category] = Relationship(back_populates="products")
+
+    # Relacionamentos com User
+    created_by: Optional["User"] = Relationship(
+        sa_relationship_kwargs={"foreign_keys": "Product.created_by_id"}
+    )
+    updated_by: Optional["User"] = Relationship(
+        sa_relationship_kwargs={"foreign_keys": "Product.updated_by_id"}
+    )
